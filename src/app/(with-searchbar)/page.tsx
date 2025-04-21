7662410a261ad2fa59c8c20bf5aa0fb40f82a666
bitnalchan92 <bitnalchan92@gmail.com>
2025-04-21 23:10:19 +0900

    day12 완료

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,33 +1,66 @@
 import style from "./page.module.css";
-import movies from '@/dummy.json';
 import {MovieData} from "@/types";
 import MovieItem from "@/components/movie-item";
 
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    {cache: 'force-cache'} // 변경사항이 적고, 캐시된 데이터를 사용하는게 적절하다고 판단!
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>
+  }
+
+  const movies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.movie_container}>
+      {
+        movies.map((movie: MovieData) => (
+          <MovieItem key={movie.id} {...movie} />
+        ))
+      }
+    </div>
+  )
+}
+
+async function RecoMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    {
+      next: {revalidate: 3} // 자주바뀌는게 좋지만 실시간느낌까진 필요 없을듯해, 3초가 지나면 캐시를 업데이트하는게 적절하다고 판단
+    }
+  )
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>
+  }
+
+  const recoMovies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.recommend_movie_container}>
+      {
+        recoMovies.map((movie: MovieData) => (
+          <MovieItem key={movie.id} {...movie} />
+        ))
+      }
+    </div>
+  )
+}
+
 export default function Home() {
+
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.recommend_movie_container}>
-          {
-            movies
-              .sort(() => Math.random() - 0.5) // 배열을 랜덤하게 섞고
-              .slice(0, 3) // 앞에서 3개만 잘라서
-              .map((movie: MovieData) => (
-                <MovieItem key={movie.id} {...movie} />
-              ))
-          }
-        </div>
+        <RecoMovies/>
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <div className={style.movie_container}>
-          {
-            movies.map((movie: MovieData) => (
-              <MovieItem key={movie.id} {...movie} />
-            ))
-          }
-        </div>
+        <AllMovies/>
       </section>
     </div>
   );
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,9 +1,18 @@
 import style from './page.module.css';
-import movies from '@/dummy.json';
 import MovieItem from "@/components/movie-item";
+import {MovieData} from "@/types";
 
 export default async function Page({searchParams}: { searchParams: Promise<{ q: string }> }) {
-  const {q} = await searchParams;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${(await searchParams).q}`,
+    {cache: 'no-cache'}
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>
+  }
+
+  const movies: MovieData[] = await response.json();
 
   return (
     <div className={style.container}>
